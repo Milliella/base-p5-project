7aedd0ce68d073351e338c90db9cf0734328b6b2
Milliella <115160750+Milliella@users.noreply.github.com>
2023-01-10 11:46:33 +0000

    animated sin wave
    animated sin wave

--- js/sketch.js
+++ js/sketch.js
@@ -1,19 +1,31 @@
+let x = 0;
+let y = 0;
+let theta = 0;
+let inc = 0.05; //the amount of increment the offset each frame
+let offset = 0; //the amount offfset from beginning of wave
+let amplitude; 
+let frequency;
+let maxAngle;
+
 function setup() {
+    amplitude = windowHeight / 8;
+    frequency = windowWidth / 8;
+    maxAngle = (windowWidth / frequency) * TWO_PI;
     createCanvas(windowWidth, windowHeight);
-    let x = 0;
-    let y = 0;
-    let theta = 0;
-    let amplitude = windowHeight/8;
-    let frequency = windowWidth/8;
-    let maxAngle = (windowWidth/frequency) * TWO_PI;
-    while(theta < maxAngle){
+    noStroke();
+    fill(255, 255, 255)
+}
+
+function draw() {
+    background(0);
+    while (theta < maxAngle + offset) {
         y = sin(theta) * amplitude;
-        ellipse(x, y + height/2, 16);
+        ellipse(x, y + height / 2, 16);
         theta += 0.2;
-        x = (theta/maxAngle) * windowWidth;
+        x = ((theta - offset) / maxAngle) * windowWidth;
 
 
     }
-}
-function draw() {
+    offset += inc;
+    theta = offset;
 }
